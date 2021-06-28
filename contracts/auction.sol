39e96e331c6e9cf09584b8a2d3dfe8d23a97cd60
ymal-bits <f20190269@goa.bits-pilani.ac.in>
2021-06-28 18:03:17 +0530

    Auction Contract Semi-working Commit

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -4,42 +4,36 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Auction {
     //VARIABLES
     uint256 public minimumBid;
-    address[] public addressIndices;
-    uint256 public previousRoundUsersCount; //To count the number of active users from the previous round (also used in the first round)
-
-    uint256 public currentRoundUsersCount;
-    uint256 roundNumber = 1;
-
-    // Arrays to store both addresses and value of bids
-    address[] addresses;
-    address[] withdrawnUsers;
-    uint256[] bidValues;
+    address payable public owner;
+    address public auctioneer;
+    address public highestBidder;
+    uint256 public highestBid = 0;
+    uint256 roundNumber;
+    uint256 public activeUsersCount;
 
     struct Bid {
         bytes32 blindedBid;
         uint256 deposit;
     }
 
-    address payable public owner;
-    address public auctioneer;
+    mapping(address => Bid[]) public bids;
+    mapping(address => uint256) pendingReturns;
 
     uint256 biddingEnd;
     uint256 revealEnd;
 
     bool public ended;
     bool public cancelled;
-    bool ownerHasWithdrawn;
 
-    mapping(address => uint256) public previousBids;
-    mapping(address => uint256) currentRoundBid; // to store the incremental bid passed in the current round
+    //address[] public addressIndices;
+    //uint256 public previousRoundUsersCount; //To count the number of active users from the previous round (also used in the first round)
+    //uint256 public currentRoundUsersCount;
+    //address[] addresses;
+    //address[] withdrawnUsers;
+    //uint256[] bidValues;
 
-    mapping(address => Bid[]) public bids;
-
-    mapping(address => uint256) pendingReturns;
-
-    address public highestBidder;
-    uint256 public highestBid = 0;
-    // uint256 public highestBindingBid; // equal to second highest bid? Not needed as highest bidder isnt allowed to withdraw
+    //mapping(address => uint256) public previousBids;
+    //mapping(address => uint256) currentRoundBid; // to store the incremental bid passed in the current round
 
     // MODIFIERS
 
@@ -47,32 +41,26 @@ contract Auction {
         require(block.timestamp < _time);
         _;
     }
-
     modifier onlyAfter(uint256 _time) {
         require(block.timestamp > _time);
         _;
     }
-
     modifier onlyAuctioneer() {
         require(msg.sender == auctioneer);
         _;
     }
-
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-
     modifier onlyNotOwner() {
         require(msg.sender != owner);
         _;
     }
-
     modifier onlyNotCancelled() {
         require(!cancelled);
         _;
     }
-
     modifier onlyNotHighestBidder() {
         require(msg.sender != highestBidder);
         _;
@@ -94,6 +82,7 @@ contract Auction {
         minimumBid = _minimumBid;
         biddingEnd = block.timestamp + _biddingTime;
         revealEnd = biddingEnd + _revealTime;
+        roundNumber = 1;
     }
 
     function generateBlindedBidBytes32(uint256 value)
@@ -102,101 +91,77 @@ contract Auction {
         returns (bytes32)
     {
         return keccak256(abi.encodePacked(value, msg.sender)); // taking the user address as the cryptographic salt
-        // THIS FUNCTION IS COMPLETE
     }
 
     function bid(bytes32 _blindedBid) public payable onlyBefore(biddingEnd) {
         bids[msg.sender].push(
             Bid({blindedBid: _blindedBid, deposit: msg.value})
-        );
+        ); // push is used as an array of structs exists and so, the newest struct is pushed into the array and forms the most recent element
     }
 
-    function reveal() public onlyAfter(biddingEnd) onlyBefore(revealEnd) {
-        // THIS FUNCTION IS INCOMPLETE
+    function revealAndPlaceBid(uint256 _value)
+        public
+        onlyAfter(biddingEnd)
+        onlyBefore(revealEnd)
+        returns (bool success)
+    {
+        // this function merges both the reveal process and the placing bid process
+        // To reveal the bid submitted by the user and place the bid (will return true if the bid is sucessfully placed)
+        // THIS FUNCTION IS COMPLETE
         uint256 length = bids[msg.sender].length;
-        require(length == roundNumber);
+        // 0x1234 => [[hdjwhksjkd, 1], [ervbcdbchebdskcb, 2]]
+        // length measures the number of total bids submitted by the user in all rounds till now (single bid per round)
+        require(length == roundNumber); // check to make sure the user has participated in all rounds of the auction till now
 
         uint256 totalBidValue;
         Bid storage bidToCheck = bids[msg.sender][length - 1];
-        uint256 value = require(
+
+        require(
             bidToCheck.blindedBid ==
-                keccak256(abi.encodePacked(value, msg.sender))
+                keccak256(abi.encodePacked(_value, msg.sender))
         );
-        uint256 random;
+
         for (uint256 i = 0; i < length; i++) {
             Bid storage roundBid = bids[msg.sender][i];
             totalBidValue += roundBid.deposit;
         }
-        pendingReturns[msg.sender] = totalBidValue;
-    }
 
-    function placeBid(address bidder, uint256 value)
-        internal
-        onlyNotCancelled
-        onlyNotOwner
-        returns (bool success)
-    {
-        //  WORK ON THIS FUNCTION - IT IS INCOMPLETE
-
-        uint256 pastBids = pendingReturns[bidder]; //pendingReturns[bidder] contains the value of the past bids made by the user
-        uint256 totalBid = pastBids + value;
-        if (totalBid <= minimumBid) {
+        //placeBid(msg.sender, totalBidValue);
+        if (totalBidValue <= minimumBid) {
             return false;
-        }
-        if (totalBid > minimumBid) {
-            pendingReturns[bidder] = totalBid;
-            if (totalBid > highestBid) {
-                highestBid = totalBid;
-                highestBidder = bidder;
+        } else {
+            // (when totalBidValue > minimumBid)
+            if (totalBidValue > highestBid) {
+                highestBid = totalBidValue;
+                highestBidder = msg.sender;
             }
+            pendingReturns[msg.sender] = totalBidValue;
+            activeUsersCount++; // this must be reset / initialized at the start of every round
             return true;
         }
-
-        // THIS FUNCTION IS INCOMPLETE
-    }
-
-    function firstRoundEnd()
-        public
-        onlyAfter(revealEnd)
-        onlyAuctioneer
-        returns (uint256 _minimumBid)
-    {
-        for (uint256 i = 0; i < addresses.length; i++) {
-            if (previousBids[addresses[i]] > highestBid) {
-                highestBid = previousBids[addresses[i]];
-                highestBidder = addresses[i];
-            }
-        }
-        minimumBid = highestBid; // set the minimum bid(for the next round) equal to the highest bid of the current round
-        return minimumBid;
     }
 
     function roundEnd() public onlyAfter(revealEnd) onlyAuctioneer {
-        // address[] storage withdrawnUsers;
-        roundNumber++;
-        for (uint256 i = 0; i < addresses.length; i++) {
-            currentRoundBid[addresses[i]] += previousBids[addresses[i]];
-            if (currentRoundBid[addresses[i]] == previousBids[addresses[i]]) {
-                withdrawnUsers.push(addresses[i]);
-            }
+        // if will be the case where one round has ended, more than 1 user remains and the auction proceeds to the next round
+        if (activeUsersCount > 1) {
+            minimumBid = highestBid;
+            activeUsersCount = 0;
+            roundNumber++;
         }
-        for (uint256 i = 0; i < addresses.length; i++) {
-            for (uint256 j = 0; j < withdrawnUsers.length; j++) {
-                if (addresses[i] == withdrawnUsers[j]) {
-                    delete addresses[i]; // this creates empty elements at the deleted places in the array -> work on shifting
-                }
-            }
+        // else will be the case where the auction has ended and only 1 user remains
+        else {
+            require(!ended);
+            emit AuctionEnded(highestBidder, highestBid);
+            ended = true;
+            // transfer the NFT to the highest bidder
+            // transfer the winning bid to the NFT owner
+            owner.transfer(highestBid);
         }
-
-        // is called only after the bid reveal process for that round is over
-        minimumBid = highestBid; //the minimum bid for the next round is equal to the highest bid of the just concluded round
-        // find the number of active users - the no. of users who participated in the just concluded round
-
-        // THIS FUNCTION IS INCOMPLETE
     }
 
     function withdraw() public onlyNotHighestBidder returns (bool success) {
         // mention when does it take place wrt the auction rounds
+        // THIS FUNCTION IS COMPLETE
         address withdrawalAccount;
         uint256 withdrawalAmount;
 
@@ -259,6 +224,73 @@ contract Auction {
         
     }
 
-    
-}
-*/
+
+    /*
+    function placeBid(address bidder, uint256 value)
+        internal
+        returns (bool success)
+    { 
+        // This function has been clubbed with the bid reveal function
+        uint256 totalBid = pendingReturns[msg.sender];
+        if (totalBid <= minimumBid) {
+            return false;
+        }
+        
+        if (totalBid > minimumBid) {
+            if (totalBid > highestBid) {
+                highestBid = totalBid;
+                highestBidder = bidder;
+            }
+            return true;
+        }
+        
+        // I can write the above commented code to find the highest bid but I do not know the order of execution of the 'internal' function
+        if (totalBid > minimumBid) {}
+
+    }
+    */
+
+/*
+    function RoundEnd()
+        public
+        onlyAfter(revealEnd)
+        onlyAuctioneer
+        returns (uint256 _minimumBid)
+    {
+        /*for (uint256 i = 0; i < addresses.length; i++) {
+            if (previousBids[addresses[i]] > highestBid) {
+                highestBid = previousBids[addresses[i]];
+                highestBidder = addresses[i];
+            }
+        }
+        */
+//minimumBid = highestBid; // set the minimum bid(for the next round) equal to the highest bid of the current round
+//return minimumBid;
+// }
+
+/*
+    function roundEnd() public onlyAfter(revealEnd) onlyAuctioneer {
+        // address[] storage withdrawnUsers;
+        roundNumber++;
+        for (uint256 i = 0; i < addresses.length; i++) {
+            currentRoundBid[addresses[i]] += previousBids[addresses[i]];
+            if (currentRoundBid[addresses[i]] == previousBids[addresses[i]]) {
+                withdrawnUsers.push(addresses[i]);
+            }
+        }
+        for (uint256 i = 0; i < addresses.length; i++) {
+            for (uint256 j = 0; j < withdrawnUsers.length; j++) {
+                if (addresses[i] == withdrawnUsers[j]) {
+                    delete addresses[i]; // this creates empty elements at the deleted places in the array -> work on shifting
+                }
+            }
+        }
+        
+
+        // is called only after the bid reveal process for that round is over
+       // minimumBid = highestBid; //the minimum bid for the next round is equal to the highest bid of the just concluded round
+        // find the number of active users - the no. of users who participated in the just concluded round
+
+        // THIS FUNCTION IS INCOMPLETE
+    }
+    */
